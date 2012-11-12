6af36b3c53b9f2503768daa5ca31c7bcfcc76a7f
janila@web.de <janila@web.de@e1ecb16e-6aa9-72c5-076a-6e0cbe709e06>
2012-11-12 13:27:58 +0000

    network: first example copied from netty

---
+++ main/src/org/iremake/client/network/Client.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2012 Trilarion
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.iremake.client.network;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+
+/**
+ *
+ */
+public class Client {
+
+    private final String host;
+    private final int port;
+    private final int firstMessageSize;
+
+    public Client(String host, int port, int firstMessageSize) {
+        this.host = host;
+        this.port = port;
+        this.firstMessageSize = firstMessageSize;
+    }
+
+    public void run() {
+        // Configure the client.
+        ClientBootstrap bootstrap = new ClientBootstrap(new NioClientSocketChannelFactory(
+                Executors.newCachedThreadPool(), Executors.newCachedThreadPool()));
+
+        // Set up the pipeline factory.
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            @Override
+            public ChannelPipeline getPipeline() throws Exception {
+                return Channels.pipeline(new ClientHandler(firstMessageSize));
+            }
+        });
+
+        // Start the connection attempt.
+        ChannelFuture future = bootstrap.connect(new InetSocketAddress(host, port));
+        
+        // Wait until the connection is closed or the connection attempt fails.
+        future.getChannel().getCloseFuture().awaitUninterruptibly();
+
+        // Shut down thread pools to exit.
+        bootstrap.releaseExternalResources();
+    }
+}

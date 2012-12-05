f8703baa7e3c4b9baa00103e034d27c8db747c3b
janila@web.de <janila@web.de@e1ecb16e-6aa9-72c5-076a-6e0cbe709e06>
2012-12-05 21:39:32 +0000

    tools: more on map import

--- tools/imperialism_map_import.py
+++ tools/imperialism_map_import.py
@@ -10,11 +10,46 @@
 # Licence:	 GPLv3
 #-------------------------------------------------------------------------------
 
+# TODO ord instead of strings in the display
+
+import array, struct
 import tools
+
+def find_unique_elements(data, n, s):
+    for x in xrange(0, 35):
+        sub = data[x : n * s + x : s]
+        unique = list(set(sub))
+        unique.sort()
+        print 'cell {0} has {1} unique values:'.format(x, len(unique))
+        print ' {0}'.format(unique)
+
+def integrity_tests(data, n, s):
+    country1 = data[3 : n * s + 3 : s]
+    country2 = data[4 : n * s + 4 : s]
+    if country1 == country2:
+        print 'cell 3 and 4 are identical'
+
+def crop_left(data, columns, col):
+    return [v for i, v in enumerate(data) if i % columns >= col]
+
+# assumes a list of string and converts to array of int
+def list_to_array(data):
+    data = map(ord, data)
+    return array.array('i', data)
+
+def correlate_unique_elements(a, b):
+    # make a unique and sorted
+    au = list(set(a))
+    au.sort()
+    for e in au:
+        print 'a: {0}'.format(e)
+        bu = [v for i, v in enumerate(b) if a[i] == e]
+        bu = list(set(bu))
+        bu.sort()
+        print ' goes with b: {0}'.format(bu)
 
 #parameters
 map_name = 's0.map'
-out_name = 'import.map'
 
 # read file
 in_file = open(map_name, 'rb')
@@ -22,68 +57,47 @@ data = in_file.read()
 in_file.close()
 
 # store interesting values for each tile cell (36 bytes long)
-n = 108 * 60;
+columns = 108
+rows = 60
+n = columns * rows;
 s = 36;
 
+# unique elements for each cell
+find_unique_elements(data, n, s)
 
-terrain = data[0: n * s : s]
-# 0	level grass	clearing, cotton, cattle, horse, town
-# 1	forest		forest
-# 2	hill		barren hills, wool
-# 3	mountain	mountain
-# 4	swamp		swamp
-# 5	ocean		ocean
-# 6	wasteland	desert, tundra
-# 7	farmland	grain, orchard
+# some equality tests
+integrity_tests(data, n, s)
 
-resources = data[17: n * s + 17 : s]
-# 0	cotton
-# 1	wool
-# 2	forest
-# 3	coal
-# 4	iron
-# 5	horses
-# 6	oil
-# 17	grain
-# 18	fruit
-# 19	fish (not used)
-# 20	cattle
-# 21	gems
-# 22	gold
-# 255	no resource
+# get interesting data out and crop left columns
+terrain_underlay = crop_left(data[0 : n * s + 0: s], columns, 8)
+terrain_overlay = crop_left(data[19 : n * s + 19: s], columns, 8)
+resources = crop_left(data[17 : n * s + 17: s], columns, 8)
 
-# trim size (first 8 columns)
-terrain = [v for i, v in enumerate(terrain) if i % 108 >= 8]
-resources = [v for i, v in enumerate(resources) if i % 108 >= 8]
+provinceA = crop_left(data[20 : n * s + 20: s], columns, 8)
+provinceB = crop_left(data[21 : n * s + 21: s], columns, 8)
+provinces = map(lambda a, b: ord(a) * 256 + ord(b), provinceA, provinceB)
 
-# some statistics
-unique_terrain = list(set(terrain))
-unique_terrain.sort()
-for t in unique_terrain:
-    print 'terrain {0}'.format(ord(t))
-    rt = [vars for i, vars in enumerate(resources) if terrain[i] == t]
-    unique_rt = list(set(rt))
-    unique_rt.sort()
-    h = [ord(v) for v in unique_rt]
-    print ' contains resources {0}'.format(h)
+# even more tests
+correlate_unique_elements(terrain_underlay, resources)
 
+# conversion to arrays
+size = array.array('i')
+size.append(100)
+size.append(60)
 
-# transform to our scheme
-new_terrain = {'\x00': 'p1', '\x01': 'f1', '\x02': 'h1', '\x03': 'm1', '\x04': 'x1', '\x05': 's1', '\x06': 'd1', '\x07': 'p1'}
+terrain_underlay = list_to_array(terrain_underlay)
+terrain_overlay = list_to_array(terrain_overlay)
+resources = list_to_array(resources)
 
-result = []
-for i in range(len(terrain)):
-    t = new_terrain[terrain[i]]
-    if resources[i] == chr(18):
-        t = 'p4'
-    if resources[i] == chr(0):
-        t = 'p3'
-    if resources[i] == chr(20):
-        t = 'p2'
-    result.append(t)
+provinces = array.array('i', provinces)
 
-# save output
-tools.write(out_name, ''.join(result))
-#out_file = open(out_name, 'wb')
-#out_file.write(''.join(result))
-#out_file.close()
+# save in file
+out_name = map_name[:-4] + '.imported.map'
+out = open(out_name, 'wb')
+out.write(size)
+out.write(terrain_underlay)
+out.write(terrain_overlay)
+out.write(resources)
+out.write(provinces)
+out.close()
+

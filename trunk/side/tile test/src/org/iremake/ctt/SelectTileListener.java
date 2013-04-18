02c7306e21a7eb61a054f1cdfbb05907d01bf695
janila@web.de <janila@web.de@e1ecb16e-6aa9-72c5-076a-6e0cbe709e06>
2013-04-18 21:36:20 +0000

    more on continuous tiles

---
+++ trunk/side/tile test/src/org/iremake/ctt/SelectTileListener.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2013 Trilarion
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.iremake.ctt;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.text.JTextComponent;
+
+/**
+ *
+ */
+class SelectTileListener implements ActionListener {
+
+    private JTextComponent textComponent;
+    private Component parent;
+    private static JFileChooser fileChooser;
+
+    {
+        fileChooser = new JFileChooser();
+        fileChooser.setMultiSelectionEnabled(false);
+        fileChooser.setFileFilter(new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                return f.isDirectory() || f.getName().endsWith(".png");
+            }
+
+            @Override
+            public String getDescription() {
+                return "Tile files (*.png)";
+            }
+        });
+    }
+
+    public SelectTileListener(JTextComponent textComponent, Component parent) {
+        this.textComponent = textComponent;
+        this.parent = parent;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (fileChooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
+            File f = fileChooser.getSelectedFile();
+            textComponent.setText(f.getPath());
+        }
+    }
+}
